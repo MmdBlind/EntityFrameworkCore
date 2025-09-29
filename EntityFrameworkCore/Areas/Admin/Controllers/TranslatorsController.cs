81df6dbcf68cf6813d920a562abb8f42b46d30b3
Blind <Blind@DESKTOP-Q6H1FK6>
2025-09-29 20:06:42 +0330

    push

---
+++ EntityFrameworkCore/Areas/Admin/Controllers/TranslatorsController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace EntityFrameworkCore.Areas.Admin.Controllers
+{
+    public class TranslatorsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
