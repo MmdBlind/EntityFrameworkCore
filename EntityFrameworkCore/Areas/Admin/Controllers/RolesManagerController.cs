06448b363a2d2885bc3ec38d40538b22c2ba7f4f
Mohammad <fathimohamad7379@gmail.com>
2025-12-23 14:58:20 +0330

    push

--- EntityFrameworkCore/Areas/Admin/Controllers/RolesManagerController.cs
+++ EntityFrameworkCore/Areas/Admin/Controllers/RolesManagerController.cs
@@ -41,25 +41,26 @@ namespace EntityFrameworkCore.Areas.Admin.Controllers
             if (ModelState.IsValid)
             {
                 var resault = await _roleManager.CreateAsync(new IdentityRole(viewModel.RoleName));
-                if(resault.Succeeded)
+                if (resault.Succeeded)
                 {
                     return RedirectToAction("Index");
                 }
 
                 ViewBag.Error = "در ذخیره اطلاعات هطایی رخ داده است.";
-                return View(viewModel );
+                return View(viewModel);
             }
             return View(viewModel);
         }
-        
+
+        [HttpGet]
         public async Task<IActionResult> EditRole(string? id)
         {
-            if(id is null)
+            if (id is null)
             {
                 return NotFound();
             }
             var Role = await _roleManager.FindByIdAsync(id);
-            if(Role is null)
+            if (Role is null)
             {
                 return NotFound();
             }
@@ -68,7 +69,36 @@ namespace EntityFrameworkCore.Areas.Admin.Controllers
                 RoleID = Role.Id,
                 RoleName = Role.Name
             };
-                return View(viewModel);
+            return View(viewModel);
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+
+        public async Task<IActionResult> EditRole(RolesViewModel viewModel)
+        {
+            if (ModelState.IsValid)
+            {
+                var Role = await _roleManager.FindByIdAsync(viewModel.RoleID);
+                if (Role is null)
+                {
+                    return NotFound();
+                }
+                Role.Name = viewModel.RoleName;
+
+                var Resault = await _roleManager.UpdateAsync(Role);
+                if (Resault.Succeeded is true)
+                {
+                    ViewBag.Message = "عملیات با موفقیت انجام شد.";
+                    return RedirectToAction("Index");
+                }
+                else
+                {
+                    ViewBag.Error = "در ذخیره اطلاعات خطایی رخ داده است.";
+                    return View(viewModel);
+                }
+            }
+            ViewBag.Error = "اطلاعات وارد شده معتبر نمی باشد.";
+            return View(viewModel);
         }
     }
 }
