e1a5deffe0d616d531c26e9e23dbe7decbfd1080
Mohammad <fathimohamad7379@gmail.com>
2025-12-26 21:10:17 +0330

    push customize IdentityRole

--- EntityFrameworkCore/Areas/Admin/Controllers/RolesManagerController.cs
+++ EntityFrameworkCore/Areas/Admin/Controllers/RolesManagerController.cs
@@ -1,4 +1,5 @@
-﻿using EntityFrameworkCore.Models.ViewModels;
+﻿using EntityFrameworkCore.Areas.Identity.Data;
+using EntityFrameworkCore.Models.ViewModels;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using ReflectionIT.Mvc.Paging;
@@ -8,8 +9,8 @@ namespace EntityFrameworkCore.Areas.Admin.Controllers
     [Area("Admin")]
     public class RolesManagerController : Controller
     {
-        private readonly RoleManager<IdentityRole> _roleManager;
-        public RolesManagerController(RoleManager<IdentityRole> roleManager)
+        private readonly RoleManager<ApplicationRole> _roleManager;
+        public RolesManagerController(RoleManager<ApplicationRole> roleManager)
         {
             _roleManager = roleManager;
         }
@@ -18,7 +19,8 @@ namespace EntityFrameworkCore.Areas.Admin.Controllers
             var Roles = _roleManager.Roles.Select(r => new RolesViewModel
             {
                 RoleID = r.Id,
-                RoleName = r.Name
+                RoleName = r.Name,
+                RoleDescription = r.Description
             }).ToList();
             var PagingModel = PagingList.Create(Roles, row, page);
             PagingModel.RouteValue = new RouteValueDictionary
@@ -40,14 +42,21 @@ namespace EntityFrameworkCore.Areas.Admin.Controllers
         {
             if (ModelState.IsValid)
             {
-                var resault = await _roleManager.CreateAsync(new IdentityRole(viewModel.RoleName));
-                if (resault.Succeeded)
+                if (await _roleManager.RoleExistsAsync(viewModel.RoleName))
                 {
-                    return RedirectToAction("Index");
+                    ViewBag.Error = "نام نقش تکراری می‌باشد.";
+                    return View();
                 }
+                else
+                {
+                    var resault = await _roleManager.CreateAsync(new ApplicationRole(viewModel.RoleName, viewModel.RoleDescription));
+                    if (resault.Succeeded)
+                    {
+                        return RedirectToAction("Index");
+                    }
 
-                ViewBag.Error = "در ذخیره اطلاعات هطایی رخ داده است.";
-                return View(viewModel);
+                    ViewBag.Error = "در ذخیره اطلاعات هطایی رخ داده است.";
+                }
             }
             return View(viewModel);
         }
@@ -67,7 +76,8 @@ namespace EntityFrameworkCore.Areas.Admin.Controllers
             RolesViewModel viewModel = new RolesViewModel
             {
                 RoleID = Role.Id,
-                RoleName = Role.Name
+                RoleName = Role.Name,
+                RoleDescription = Role.Description
             };
             return View(viewModel);
         }
@@ -82,59 +92,68 @@ namespace EntityFrameworkCore.Areas.Admin.Controllers
                 {
                     return NotFound();
                 }
-                Role.Name = viewModel.RoleName;
-
-                var Resault = await _roleManager.UpdateAsync(Role);
-                if (Resault.Succeeded is true)
+                if(await _roleManager.RoleExistsAsync(viewModel.RoleName))
                 {
-                    ViewBag.Message = "عملیات با موفقیت انجام شد.";
-                    return RedirectToAction("Index");
+                    ViewBag.Message = "نام نقش تکراری می‌باشد.";
+                    return View(viewModel);
                 }
                 else
                 {
-                    ViewBag.Error = "در ذخیره اطلاعات خطایی رخ داده است.";
-                    return View(viewModel);
+                    Role.Name = viewModel.RoleName;
+                    Role.Description = viewModel.RoleDescription;
+
+                    var Resault = await _roleManager.UpdateAsync(Role);
+                    if (Resault.Succeeded is true)
+                    {
+                        ViewBag.Message = "عملیات با موفقیت انجام شد.";
+                        return RedirectToAction("Index");
+                    }
+                    else
+                    {
+                        ViewBag.Error = "در ذخیره اطلاعات خطایی رخ داده است.";
+                        return View(viewModel);
+                    }
                 }
             }
             ViewBag.Error = "اطلاعات وارد شده معتبر نمی باشد.";
             return View(viewModel);
         }
-        
+
         [HttpGet]
         public async Task<IActionResult> DeleteRole(string id)
         {
-            if(id == null)
+            if (id == null)
             {
                 return NotFound();
             }
             var Role = await _roleManager.FindByIdAsync(id);
-            if(Role==null)
-            {
-                return NotFound();
-            }
-            RolesViewModel viewModel = new RolesViewModel 
-            {
-                RoleID= Role.Id,
-                RoleName=Role.Name
-            };
-            return View(viewModel);
-        }
-
-        [HttpPost,ActionName("DeleteRole")]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> DeletedRole(string id)
-        {
-            if(id==null)
-            {
-                return NotFound();
-            }
-            var Role= await _roleManager.FindByIdAsync(id);
             if (Role == null)
             {
                 return NotFound();
             }
-            var Resault =await _roleManager.DeleteAsync(Role);
-            if(Resault.Succeeded)
+            RolesViewModel viewModel = new RolesViewModel
+            {
+                RoleID = Role.Id,
+                RoleName = Role.Name
+            };
+            return View(viewModel);
+        }
+
+        [HttpPost, ActionName("DeleteRole")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeletedRole(string id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+            var Role = await _roleManager.FindByIdAsync(id);
+            if (Role == null)
+            {
+                return NotFound();
+            }
+            var Resault = await _roleManager.DeleteAsync(Role);
+            if (Resault.Succeeded)
             {
                 ViewBag.Message = "عملیات با موفقیت انجام شد.";
                 return RedirectToAction("Index");
@@ -148,10 +167,10 @@ namespace EntityFrameworkCore.Areas.Admin.Controllers
                 };
 
                 ViewBag.Message = "عملیات با شکست مواجه شد";
-                
+
                 return View(viewModel);
             }
-                
+
         }
     }
 }
--- EntityFrameworkCore/Models/ViewModels/RolesManagerViewModel.cs
+++ EntityFrameworkCore/Models/ViewModels/RolesManagerViewModel.cs
@@ -9,5 +9,8 @@ namespace EntityFrameworkCore.Models.ViewModels
         [Display(Name = "عنوان نقش")]
         [Required(ErrorMessage ="وارد نمودن {0} الزامی است.")]
         public string RoleName { get; set; }
+        [Display(Name ="توضیحات نقش")]
+        [Required(ErrorMessage ="وارد نمودن {0} الزامی است")]
+        public string RoleDescription { get; set; }
     }
 }
